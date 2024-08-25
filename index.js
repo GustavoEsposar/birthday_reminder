700780a508e7af6265cac1144bcc3e12dce6eb3b
Gustavo Dektop-Ubuntu <guesposar@icloud.com>
2024-08-25 15:57:31 -0300

    deploy v1.0

--- index.js
+++ index.js
@@ -13,7 +13,6 @@ const MONGOOSE_OPTIONS = { useNewUrlParser: true, useUnifiedTopology: true };
 mongoose.connect(process.env.MONGODB_URI, MONGOOSE_OPTIONS)
     .then(() => {
         console.log('Conectado ao MongoDB com sucesso');
-        enviarLembretePorEmail(0, " - HOJE");
     })
     .catch((error) => {
         console.log('Erro ao conectar ao MongoDB:', error);
