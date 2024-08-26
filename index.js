15741b414aba7d4efd9551753166f7102062943d
Gustavo Dektop-Ubuntu <guesposar@icloud.com>
2024-08-26 14:57:26 -0300

    version 1.1

--- index.js
+++ index.js
@@ -13,38 +13,24 @@ const MONGOOSE_OPTIONS = { };
 mongoose.connect(process.env.MONGODB_URI, MONGOOSE_OPTIONS)
     .then(() => {
         console.log('Conectado ao MongoDB com sucesso');
-        enviarLembretePorEmail(0, " - HOJE")
+        enviarLembretePorEmail(0, " - HOJE");
+        enviarLembretePorEmail(2, " - Em 2 dias");
+        enviarLembretePorEmail(7, " - Em 7 dias");
     })
     .catch((error) => {
         console.log('Erro ao conectar ao MongoDB:', error);
     });
 
 app.get('/', async (req, res) => {
-    // Carregar lista de aniversários, verificar quem faz aniversário hoje
-    const today = new Date();
-    const month = today.getUTCMonth() + 1;
-    const day = today.getUTCDate();
-
-    const aniversarios = await Pessoa.find({
-        $expr: {
-            $and: [
-                { $eq: [{ $month: "$date" }, month] },
-                { $eq: [{ $dayOfMonth: "$date" }, day] }
-            ]
-        }
-    }).sort({ date: 1 });
-
-    if (aniversarios.length > 0) {        
-        await enviarLembretePorEmail(0, " - HOJE");
-        res.send(`Emails enviados para os aniversariantes de hoje.`);
-    } else {
-        res.send('Nenhum aniversário encontrado para hoje.');
-    }
+    enviarLembretePorEmail(0, " - HOJE");
+    enviarLembretePorEmail(2, " - Em 2 dias");
+    enviarLembretePorEmail(7, " - Em 7 dias");
 });
 
 cron.schedule('0 5 * * *', async () => {
     await enviarLembretePorEmail(0, " - HOJE");
     await enviarLembretePorEmail(2, " - Em 2 dias");
+    await enviarLembretePorEmail(7, " - Em 7 dias");
 });
 
 app.listen(PORT, () => {
--- services/emailService.js
+++ services/emailService.js
@@ -13,44 +13,55 @@ const transporter = nodemailer.createTransport({
 
 const enviarLembretePorEmail = async (intervalo, mensagem) => {
     const hoje = new Date();
+    hoje.setDate(hoje.getDate() + intervalo)
 
     const month = hoje.getUTCMonth() + 1; // Mês é zero-based
-    const day = hoje.getUTCDate();
+    const day = hoje.getUTCDate();    
 
     // Busca aniversariantes do dia de hoje
     const aniversarios = await Pessoa.find({
         $expr: {
             $and: [
                 { $eq: [{ $month: "$date" }, month] },
-                { $eq: [{ $dayOfMonth: "$date" }, day + intervalo] }
+                { $eq: [{ $dayOfMonth: "$date" }, day] }
             ]
         }
     }).sort({ date: 1 }); // Ordena por data
 
     // Carregar o conteúdo HTML
-    const templatePath = path.join(__dirname, '../templates/todayTemplate.html');
+    let templatePath;
+    if(intervalo == 0) {
+        templatePath = path.join(__dirname, '../templates/todayTemplate.html');
+    } else {
+        templatePath = path.join(__dirname, '../templates/fewDaysTemplate.html');
+    }
     const template = fs.readFileSync(templatePath, 'utf-8');
 
-    aniversarios.forEach((aniversario) => {
-        // Substituir as variáveis do template
-        const htmlContent = template
-            .replace('{{name}}', aniversario.name);
-
-        const mailOptions = {
-            from: process.env.EMAIL,
-            to: process.env.RECIPIENT,
-            subject: 'Birthday Reminder 📅🎊' + mensagem,
-            html: htmlContent
-        };
-
-        transporter.sendMail(mailOptions, (error, info) => {
-            if (error) {
-                console.log(`Error: ${error}`);
-            } else {
-                console.log(`Email enviado para ${aniversario.name}: ${info.response}`);
-            }
+    if (aniversarios.length > 0) {        
+        aniversarios.forEach((aniversario) => {
+            // Substituir as variáveis do template
+            const htmlContent = template
+                .replace('{{name}}', aniversario.name)
+                .replace('{{days}}', intervalo);
+    
+            const mailOptions = {
+                from: process.env.EMAIL,
+                to: process.env.RECIPIENT,
+                subject: 'Birthday Reminder 📅🎊' + mensagem,
+                html: htmlContent
+            };
+    
+            transporter.sendMail(mailOptions, (error, info) => {
+                if (error) {
+                    console.log(`Error: ${error}`);
+                } else {
+                    console.log(`Email enviado para ${aniversario.name}: ${info.response}`);
+                }
+            });
         });
-    });
+    } else {
+        console.log(`Nenhum aniversário encontrado para intervalo de ${intervalo} dias`);
+    }
 };
 
 module.exports = { enviarLembretePorEmail };
