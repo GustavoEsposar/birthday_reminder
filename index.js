28e94a09c65c2f655d5fce24255006e49c9a593f
Gustavo Dektop-Ubuntu <guesposar@icloud.com>
2024-08-26 15:20:50 -0300

    deploy v1.2

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cron = require('node-cron');
+const timezone = 'America/Sao_Paulo'; 
 require('dotenv').config();
 
 const { enviarLembretePorEmail } = require('./services/emailService');
@@ -13,9 +14,9 @@ const MONGOOSE_OPTIONS = { };
 mongoose.connect(process.env.MONGODB_URI, MONGOOSE_OPTIONS)
     .then(() => {
         console.log('Conectado ao MongoDB com sucesso');
-        enviarLembretePorEmail(0, " - HOJE");
-        enviarLembretePorEmail(2, " - Em 2 dias");
-        enviarLembretePorEmail(7, " - Em 7 dias");
+        //enviarLembretePorEmail(0, " - HOJE");
+        //enviarLembretePorEmail(2, " - Em 2 dias");
+        //enviarLembretePorEmail(7, " - Em 7 dias");
     })
     .catch((error) => {
         console.log('Erro ao conectar ao MongoDB:', error);
@@ -27,10 +28,12 @@ app.get('/', async (req, res) => {
     //enviarLembretePorEmail(7, " - Em 7 dias");
 });
 
-cron.schedule('15 15 * * *', async () => {
+cron.schedule('30 15 * * *', async () => {
     await enviarLembretePorEmail(0, " - HOJE");
     await enviarLembretePorEmail(2, " - Em 2 dias");
     await enviarLembretePorEmail(7, " - Em 7 dias");
+}, {
+    timezone: timezone
 });
 
 app.listen(PORT, () => {
