4312143200e4f85f73d77398b18e9fcfc63d4374
Gustavo Dektop-Ubuntu <guesposar@icloud.com>
2024-09-03 19:43:14 -0300

    remodelação da classe lógica Pessoas para suportar modelo de dados de login

--- models/Pessoa.js
+++ models/Pessoa.js
@@ -1,11 +1,40 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const pessoaSchema = new mongoose.Schema({
-    email: String,
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        requiered: true
+    },
     birthdates: [{
         name: String,
-        date: Date
+        date: Date,
+        required: true
     }]
 }, { collection: 'contas' });
+
+// Middleware para criptografar a senha antes de salvar
+pessoaSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) {
+        return next();
+    }
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+});
+
+// Método para comparar a senha fornecida com a senha armazenada
+pessoaSchema.methods.matchPassword = function (enteredPassword) {
+    return bcrypt.compare(enteredPassword, this.password);
+};
 
 module.exports = mongoose.model('Pessoa', pessoaSchema);
